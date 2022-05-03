9dde3cebfd2eb89f49e12241cf7fee7cc21ffabc
thomsva <thomsva@gmail.com>
2022-05-03 14:35:20 +0300

    Combined e2e test to same file

--- cypress/integration/pokepage.js
+++
@@ -1,7 +0,0 @@
-describe('Pokepage', function () {
-  it('Pokemon page can be opened', function() {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
-  })
-})
