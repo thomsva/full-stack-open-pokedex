7963c8cc3cc880c59a7cfd8c494d815c54926175
thomsva <thomsva@gmail.com>
2022-05-03 14:11:45 +0300

    Added Cypress

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+if (process.env.NODE_ENV === 'test') {
+  // eslint-disable-next-line no-console
+  console.log('started in test mode')
+}
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
